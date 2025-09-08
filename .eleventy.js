0e3190df835ca518b046ddb165d1df21fe222cb1
Test User <cahaseler@gmail.com>
2025-09-08 12:01:15 -0400

    Remove Netlify dependencies and add PagesCMS redirect
    - Remove Netlify Identity widget and scripts from base template
    - Delete old Netlify CMS admin folder and config
    - Remove Netlify functions and configuration files
    - Add /admin redirect to PagesCMS (app.pagescms.org)
    - Clean up Eleventy config to remove deleted paths

--- .eleventy.js
+++ .eleventy.js
@@ -17,8 +17,6 @@ export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/css");
   eleventyConfig.addPassthroughCopy("src/assets/js");
   eleventyConfig.addPassthroughCopy("src/assets/images");
-  eleventyConfig.addPassthroughCopy("src/admin");
-  eleventyConfig.addPassthroughCopy({ "src/_redirects": "_redirects" });
 
   // Collections
   eleventyConfig.addCollection("home", function(collection) {
